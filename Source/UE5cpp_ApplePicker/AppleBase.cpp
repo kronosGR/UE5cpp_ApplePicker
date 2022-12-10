da06c55d06815be7183c99073b35071185a474bb
KronosGR <geo.elgeo@gmail.com>
2022-12-10 06:21:56 +0100

    added apple base class and BP

---
+++ Source/UE5cpp_ApplePicker/AppleBase.cpp
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c32935451c6246b0da9db39f108f4f7599ae06a4d34902d185acb85fc646c172
+size 674
---
+++ Source/UE5cpp_ApplePicker/AppleBase.h
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b0a559ebbb1a6d6df4d1e2f6e8fa2ff3537235c630ad2c39719bbd53757bb545
+size 676
