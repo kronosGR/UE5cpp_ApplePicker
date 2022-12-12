e32e71ebeadbead0b417ae511e24fefe278e7993
KronosGR <geo.elgeo@gmail.com>
2022-12-12 06:11:24 +0100

    added custom level bp script
    to set up the view target on the player controller

---
+++ Source/UE5cpp_ApplePicker/LevelScriptBase.cpp
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:3644b700a9c7a0c07897bff6baa6bfa24b3257edd60c1013c8d5eafed8526535
+size 427
---
+++ Source/UE5cpp_ApplePicker/LevelScriptBase.h
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:9777120b47d9e6a0a7e60a6a50539d6d1935fb1784d379e6c432bc9683b21670
+size 565
