4b65f65d4e3e1cf0a3106a0432b10c00bdadc177
KronosGR <geo.elgeo@gmail.com>
2022-12-13 06:06:51 +0100

    destroy apple on touch

--- Source/UE5cpp_ApplePicker/BasketBase.cpp
+++ Source/UE5cpp_ApplePicker/BasketBase.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:18d766f255493d1b727342f142994e3f0624397c19d1a3251a7326789c66f5cc
-size 1428
+oid sha256:c393a20214b1d0b90d720e0c795db0be80ab0b30c41e4f690a66e2e638f8efce
+size 1799
