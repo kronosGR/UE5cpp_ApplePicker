74cd1c4a2e04875f70aa59e97c84be1067836884
KronosGR <geo.elgeo@gmail.com>
2022-12-13 05:53:54 +0100

    added basket movement

--- Source/UE5cpp_ApplePicker/BasketBase.h
+++ Source/UE5cpp_ApplePicker/BasketBase.h
@@ -1,36 +1,3 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#pragma once
-
-#include "CoreMinimal.h"
-#include "GameFramework/Pawn.h"
-#include "BasketBase.generated.h"
-
-UCLASS()
-class UE5CPP_APPLEPICKER_API ABasketBase : public APawn
-{
-	GENERATED_BODY()
-
-public:
-	// Sets default values for this pawn's properties
-	ABasketBase();
-
-protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
-
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
-	USceneComponent* Root;
-
-	
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
-	UStaticMeshComponent* Paddle1;
-
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
-
-	// Called to bind functionality to input
-	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
-
-};
+version https://git-lfs.github.com/spec/v1
+oid sha256:d16c3879205a868e05ad743567f9238c303d59b54c080e5f493df916623964e5
+size 1070
