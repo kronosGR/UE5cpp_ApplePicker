3da190e86df797e24a4dd9920db073654eb98757
KronosGR <geo.elgeo@gmail.com>
2022-12-15 05:45:52 +0100

    paddle destruction handling

--- Source/UE5cpp_ApplePicker/BasketBase.cpp
+++ Source/UE5cpp_ApplePicker/BasketBase.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a23cc9d8a64adcc72fca826d7cf8eb509f44c843a732a3fa8636f9411d508d29
-size 2491
+oid sha256:5ca2f52f2ee5c6e4bd02adf1d86231792afd3da8a99fb347535046c88065e6e6
+size 2807
--- Source/UE5cpp_ApplePicker/BasketBase.h
+++ Source/UE5cpp_ApplePicker/BasketBase.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fafb596226a96a64f795456c126f49fbb4da06a43e2a9483e2e0f515abe92072
-size 1599
+oid sha256:13bdf26839bc12bbe2a76642e104276b89d2c2f51b78eaf8fed1569dc202deaa
+size 1682
